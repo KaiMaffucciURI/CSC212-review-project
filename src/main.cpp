e37354bafd189d4512def4734e6af7116a48c683
Syd <syd_phillips@uri.edu>
2023-02-20 14:19:38 -0500

    add todo list

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 
 #include "gradebook.h"
 
+/*
+To Do:
+Add error throwing for add, modify, delete entry
+Implement save and load
+Add user I/O
+*/
+
 int main()
 {
 	//User i/o loop should be performed here
