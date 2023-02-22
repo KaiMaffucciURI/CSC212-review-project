fc6a046f2f9ef44514732d34f0a0af4fa5cbbd7b
KaiMaffucciURI <kai_maffucci@uri.edu>
2023-02-22 17:40:41 -0500

    Put in (most) function calls based on user input: there's a few questions in the code, mainly the one around line 106, if you want to take a look

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 
-    Gradebook grades = new Gradebook();
+    Gradebook book = new Gradebook();
 
     // plan: loop which prompts the user with a list of options
     // which option they pick determines which
@@ -24,6 +24,7 @@ int main()
         std::cout << "view <type>\n\tdisplays all assignments of type <type>; if no <type> given, displays all assignments" << std::endl;
         std::cout << "modify <index> <grade> <total>\n\tmodify grade at <index> with <grade> and <total>" << std::endl;
         std::cout << "modify all\n\tlists all grades, then prompts user for an index, grade, and total the grade is out of" << std::endl;
+        std::cout << "delete <type> <name>\n\tdeletes all entries of type <type> under name <name>"
         std::cout << "exit\n\tclose the program" << std::endl;
 
         // get user's choice and parse it
@@ -41,23 +42,101 @@ int main()
         if (command_args[0] == "new") {
 
             // create new student
+            book.createStudent(command_args[1]);
 
         } else if (command_args[0] == "load") {
 
             // load student into memory from file as current working student
+            book.loadStudent(command_args[1]);
 
         } else if (command_args[0] == "add") {
 
             // add new assignment
+            if (command_args[1] == "labs") {
+                book.addEntry(labs, std::atoi(command_args[2]), std::atoi(command_args[3]));
+            } else if (command_args[1] = "assignments") {
+                book.addEntry(assignments, std::atoi(command_args[2]), std::atoi(command_args[3]));
+            } else if (command_args[1] = "projects") {
+                book.addEntry(projects, std::atoi(command_args[2]), std::atoi(command_args[3]));
+            } else if (command_args[1] = "exams") {
+                book.addEntry(exams, std::atoi(command_args[2]), std::atoi(command_args[3]));
+            } else {
+                // bad argument(s)
+                std::cout << "you suck" << std::endl; // we can put something else here
+            }
 
         } else if (command_args[0] == "view") {
 
-            // view assignments
+            // view all assignments of type command_args[1]
+            // might be nice to make a helper function which converts string to type somehow,
+            // so we don't have to go through this each time, if that's possible
+            if (command_args[1] == "labs") {
+                book.printCategory(labs);
+            } else if (command_args[1] = "assignments") {
+                book.printCategory(assignments);
+            } else if (command_args[1] = "projects") {
+                book.printCategory(projects);
+            } else if (command_args[1] = "exams") {
+                book.printCategory(exams);
+            } else {
+                // bad argument(s)
+                std::cout << "you suck" << std::endl; // we can put something else here
+            }
 
         } else if (command_args[0] == "modify") {
 
             // modify existing assignments
             // two different branches for each possible args given to modify
+            if (command_args[1] == "all") {
+
+                // print out all stuff and prompt for which one they'd like to modify
+                book.printCourse(0);
+                std::string user_in;
+                std::cin >> line;
+
+                // parse user input
+                command_args.clear();
+                std::stringstream string_stream(line);
+                std::string part;
+                while (getline(string_stream, part, ' ')) {
+                    command_args.push_back(part);
+                }
+
+                // call modifyEntry based on parsed user input
+                // there's no modifyEntry call that calls based on an index like in the example diagram:
+                // what do you prefer me to do here?
+
+            } else if (command_args[0] == "delete") {
+
+                // delete entry
+                if (command_args[1] == "labs") {
+                    book.deleteEntry(labs, command_args[2]);
+                } else if (command_args[1] = "assignments") {
+                    book.deleteEntry(assignments, command_args[2]);
+                } else if (command_args[1] = "projects") {
+                    book.deleteEntry(projects, command_args[2]);
+                } else if (command_args[1] = "exams") {
+                    book.deleteEntry(exams, command_args[2]);
+                } else {
+                    // bad argument(s)
+                    std::cout << "you suck" << std::endl; // we can put something else here
+
+            } else {
+
+                // just call modify normally, depending on type
+                if (command_args[1] == "labs") {
+                    book.modifyEntry(labs, std::atoi(command_args[2]), std::atoi(command_args[3]));
+                } else if (command_args[1] = "assignments") {
+                    book.modifyEntry(assignments, std::atoi(command_args[2]), std::atoi(command_args[3]));
+                } else if (command_args[1] = "projects") {
+                    book.modifyEntry(projects, std::atoi(command_args[2]), std::atoi(command_args[3]));
+                } else if (command_args[1] = "exams") {
+                    book.modifyEntry(exams, std::atoi(command_args[2]), std::atoi(command_args[3]));
+                } else {
+                    // bad argument(s)
+                    std::cout << "you suck" << std::endl; // we can put something else here
+                }
+            }
 
         } else if (option == "exit" || option == "quit") {
 
@@ -71,6 +150,8 @@ int main()
             std::cout << "Invalid option: please choose a valid option." << std::endl;
 
         }
+
+        command_args.clear();
     }
 
 	return 0;
